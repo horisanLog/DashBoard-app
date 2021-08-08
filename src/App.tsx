9be665a6656a2d3b0c943fe4551f451ae6db0456
horisanLog <ttie7374@gmail.com>
2021-08-08 16:16:50 +0900

    LinePlot

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react"
 import "./App.css"
+import BarPlot from "./components/BarPlot"
+import BubblePlot from "./components/BubblePlot"
 import LinePlot from "./components/LinePlot"
+import PiePlot from "./components/PiePlot"
+import RadarPlot from "./components/RadarPlot"
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <LinePlot />
+      <PiePlot />
+      <BarPlot />
+      <RadarPlot />
+      <BubblePlot />
     </div>
   )
 }
